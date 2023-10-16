520c25c31b75a9ed38a97993bdbb8ca71c720275
AlexRxPro <146444390+AlexRxPro@users.noreply.github.com>
2023-10-16 17:14:40 +0200

    update
    update app-firewall

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1 align="left">F5 Distributed Cloud Services (aka XC) - Terraform - Template - App-Firewall</h1>
 
 ###
-<p align="center">
+<p align="left">
 
 <a href="https://www.f5.com/cloud">
 <img src="https://img.shields.io/badge/F5-%20distributed%20cloud%20Services-blue?logo=f5&logoColor=white%20Edit%20this%20page%20SimpleIcons%20Shields%20logos%20Custom%20Logos" />
@@ -15,11 +15,9 @@
 <img src="https://img.shields.io/badge/Volterra%20Providers-v0.11.26-brightgreen.svg" />
 </a>
 
-
-
 </p>
 
-<p align="left">In this project, you'll find a complete example of how to create an app-firewall object for provisioning using the Terraform tool.</p>
+<p align="left">🎯 Project Goals: In this project, you'll find a complete example of how to create an app-firewall object for provisioning using the Terraform tool.</p>
 
 ###
 
@@ -27,19 +25,7 @@
 
 ###
 
-<p align="left">✨ Creating bugs since ...<br>📚 I'm currently learning ...<br>🎯 Goals: ...<br>🎲 Fun fact: ...</p>
-
-###
-
-<h2 align="left">I code with</h2>
-
-###
-
-<div align="left">
-  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/terraform/terraform-original.svg" height="40" alt="terraform logo"  />
-  <img width="12" />
-  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" height="40" alt="bash logo"  />
-</div>
+<p align="left">Terraform : version 1.6.0 <br>Provider Volterra : v0.11.26 </p>
 
 ###
 
@@ -48,5 +34,8 @@
 ###
 Documentation for provider  - https://registry.terraform.io/providers/volterraedge/volterra/latest/docs
 App Delivery Network Module - https://registry.terraform.io/modules/volterraedge/app-delivery-network/volterra/latest
-Web App Security Module - https://registry.terraform.io/modules/volterraedge/web-app-security/volterra/latest
+Web App Security Module -     https://registry.terraform.io/modules/volterraedge/web-app-security/volterra/latest
+
+API Documentation :  https://registry.terraform.io/providers/volterraedge/volterra/0.11.26/docs/resources/volterra_app_firewall
+Provider source code :  https://github.com/volterraedge/terraform-provider-volterra/blob/main/volterra/resource_auto_volterra_app_firewall.go
 ###
--- main.tf
+++ main.tf
@@ -1,89 +1,142 @@
 
 
+//                                     .8888b oo                                       dP dP
+//                                     88   "                                          88 88
+// .d8888b. 88d888b. 88d888b.          88aaa  dP 88d888b. .d8888b. dP  dP  dP .d8888b. 88 88
+// 88'  `88 88'  `88 88'  `88 88888888 88     88 88'  `88 88ooood8 88  88  88 88'  `88 88 88
+// 88.  .88 88.  .88 88.  .88          88     88 88       88.  ... 88.88b.88' 88.  .88 88 88
+// `88888P8 88Y888P' 88Y888P'          dP     dP dP       `88888P' 8888P Y8P  `88888P8 dP dP
+//          88       88
+//          dP       dP Template by alexRxpro - 10/2003
+// Template : volterra_app_firewall 
+// 
 
+
+// -- Provider 
 resource "volterra_app_firewall" "waf1" {
 
-// https://registry.terraform.io/providers/volterraedge/volterra/0.11.26/docs/resources/volterra_app_firewall
-// https://github.com/volterraedge/terraform-provider-volterra/blob/main/volterra/resource_auto_volterra_app_firewall.go
+// -- METADATA 
+// --- Name
+name                     = "ara-acmecorp-waf1"
+// --- Labels = {KeyName = Value}
+labels = { 
+   label-name = "value"
+  }
+// --- Namespace
+namespace                = var.f5_xc_namespace
+// --- Description
+description              = "Politique WAF 01"
 
+// --- Enforcement mode 
+// --- > One of the arguments from this list "use_loadbalancer_setting = true or blocking=true or monitoring=true" must be set
+# use_loadbalancer_setting   = true
+# monitoring                 = true
+blocking                     = true
 
-// ### METADATA 
-  name                     = "ara-acmecorp-waf1"
-   // INFO : labels = {KeyName = Value}
-  labels = { 
-             label-name = "value"
-           }
-  namespace                = var.f5_xc_namespace
-  description              = "Politique WAF 01"
+// --- Detection Setting 
+// --- > One of the arguments from this list "default_detection_settings=true or  detection_settings" must be set
 
-// ### ENFORCEMENT Mode 
-  // INFO : One of the arguments from this list "use_loadbalancer_setting = true or blocking=true or monitoring=true" must be set
-  blocking                     = true
+# default_detection_settings = true
 
-
-// ### Detection Setting 
-  // One of the arguments from this list "default_detection_settings detection_settings" must be set
-  #default_detection_settings = true
-  detection_settings {
-
-  // Attack Signatures
-    signature_selection_setting {
-      attack_type_settings { // or default_attack_type_settings = true 
-                              disabled_attack_types = ["ATTACK_TYPE_AUTHENTICATION_AUTHORIZATION_ATTACKS","ATTACK_TYPE_COMMAND_EXECUTION"]
-                           }
-      // Signature Selection by Accuracy
-	    high_medium_low_accuracy_signatures = true // high_medium_accuracy_signatures=true or only_high_accuracy_signatures=true
+detection_settings {
+// -- Attack Signatures
+  signature_selection_setting {
+#     default_attack_type_settings = true   
+      attack_type_settings { 
+         disabled_attack_types = ["ATTACK_TYPE_AUTHENTICATION_AUTHORIZATION_ATTACKS","ATTACK_TYPE_COMMAND_EXECUTION"]
+        }
+// -- Signature Selection by Accuracy
+#     high_medium_accuracy_signatures=true 
+#     only_high_accuracy_signatures=true
+	    high_medium_low_accuracy_signatures = true 
     }
 
-   // Automatic Attack Signautres Tuning
-   enable_suppression= true // or disable_suppression=true
+// --- Automatic Attack Signautres Tuning
+#  disable_suppression=true
+enable_suppression= true 
 
-  // Attack Signature Staging
-  // - disable_staging 
-   disable_staging=true
+// --- Attack Signature Staging 
+// -- disable_staging 
+disable_staging=true
 
-  // - stage_new_signatures
-  // stage_new_signatures {
-  //   staging_period = 7
-  // }
+// --  or stage_new_signatures
+# stage_new_signatures {
+#   staging_period = 7
+#  }
 
-  // - stage_new_and_updated_signatures
-  // stage_new_and_updated_signatures {
-  //  staging_period = 7
-  // }
+// -- or stage_new_and_updated_signatures
+# stage_new_and_updated_signatures {
+#   staging_period = 7
+#  }
 
-// Threat Campaigns
-enable_threat_campaigns = true // or disable_threat_campaigns=true 
+// --- Threat Campaigns
+enable_threat_campaigns = true 
+#disable_threat_campaigns=true 
 
-// Violations
-// default_violation_settings = true
+// --- Violations
+#default_violation_settings = true
 
 violation_settings {
-    disabled_violation_types = ["VIOL_EVASION_BARE_BYTE_DECODING","VIOL_HTTP_PROTOCOL_CONTENT_LENGTH_SHOULD_BE_A_POSITIVE_NUMBER"]
+  disabled_violation_types = ["VIOL_EVASION_BARE_BYTE_DECODING","VIOL_HTTP_PROTOCOL_CONTENT_LENGTH_SHOULD_BE_A_POSITIVE_NUMBER"]
+ } 
 
-}
+} // end detection_Setting
+// --- Bot Setting
+// --- > One of the arguments from this list "default_bot_setting bot_protection_setting" must be set
+#default_bot_setting = true
+ 
+// Custom setting and value : REPORT / BLOCK / IGNORE
+bot_protection_setting {
+  good_bot_action         = "REPORT"
+  malicious_bot_action    = "BLOCK"
+  suspicious_bot_action   = "IGNORE"
+ }
 
+// --- Advanced configuration 
 
-  // One of the arguments from this list "default_bot_setting bot_protection_setting" must be set
-  //  default_bot_setting = true
-  // Custom value : REPORT / BLOCK / IGNORE
-   bot_protection_setting {
-    good_bot_action         = "REPORT"
-    malicious_bot_action    = "BLOCK"
-    suspicious_bot_action   = "IGNORE"
-   }
+// --- Allowed Response Status Codes 
+// One of the arguments from this list "allow_all_response_codes allowed_response_codes" must be set
+allow_all_response_codes = true
 
-  // One of the arguments from this list "allow_all_response_codes allowed_response_codes" must be set
-  allow_all_response_codes = true
+#allowed_response_codes { 
+#  response_code = [200,201,202,203,204,205,206,300,301,302,303,304,308,307,400,401,403,404,407,429,500,501,502,503]
+# }
 
-  // One of the arguments from this list "disable_anonymization default_anonymization custom_anonymization" must be set
-  default_anonymization     = true
+// --- Mask Sensitive Parameters in Logs
+// ---> One of the arguments from this list "disable_anonymization default_anonymization custom_anonymization" must be set
+# disable_anonymization  = true
 
-  // One of the arguments from this list "use_default_blocking_page blocking_page" must be set
-  use_default_blocking_page = true
+default_anonymization  = true
+#custom_anonymization {
+#  anonymization_config {
+#     query_parameter {
+#       query_param_name="query1"
+#       }
+#   }
+#   anonymization_config {
+#     http_header {
+#       header_name = "header1"
+#       }
+#   }
+#   anonymization_config {
+#     cookie {
+#       cookie_name="cookie1"
+#       }  
+#     } 
+# }
 
-  
-  
-  
-}
+// --- Blocking Response Page
+// --- > One of the arguments from this list "use_default_blocking_page blocking_page" must be set
+use_default_blocking_page = true
+
+// --- Blocking_page = "string:///BASE-64-OF-YOUR-CODE-HTML"
+// SAMPLE HTML CODE = <title>Request Rejected</title></head><body>The requested URL was rejected. Please consult with your administrator.<br/><br/>Your support ID is: {{request_id}}<br/><br/><a href="javascript:history.back()">[Go Back]</a></body></html>
+// SAMPLE BASE64 = PHRpdGxlPlJlcXVlc3QgUmVqZWN0ZWQ8L3RpdGxlPjwvaGVhZD48Ym9keT5UaGUgcmVxdWVzdGVkIFVSTCB3YXMgcmVqZWN0ZWQuIFBsZWFzZSBjb25zdWx0IHdpdGggeW91ciBhZG1pbmlzdHJhdG9yLjxici8+PGJyLz5Zb3VyIHN1cHBvcnQgSUQgaXM6IHt7cmVxdWVzdF9pZH19PGJyLz48YnIvPjxhIGhyZWY9ImphdmFzY3JpcHQ6aGlzdG9yeS5iYWNrKCkiPltHbyBCYWNrXTwvYT48L2JvZHk+PC9odG1sPg==
+// /!\ Response body can't exceed 4 KB in size.
+
+#blocking_page {
+#  response_code = "OK"
+#  blocking_page = "string:///PHRpdGxlPlJlcXVlc3QgUmVqZWN0ZWQ8L3RpdGxlPjwvaGVhZD48Ym9keT5UaGUgcmVxdWVzdGVkIFVSTCB3YXMgcmVqZWN0ZWQuIFBsZWFzZSBjb25zdWx0IHdpdGggeW91ciBhZG1pbmlzdHJhdG9yLjxici8+PGJyLz5Zb3VyIHN1cHBvcnQgSUQgaXM6IHt7cmVxdWVzdF9pZH19PGJyLz48YnIvPjxhIGhyZWY9ImphdmFzY3JpcHQ6aGlzdG9yeS5iYWNrKCkiPltHbyBCYWNrXTwvYT48L2JvZHk+PC9odG1sPg=="
+#}
+} // end provider 
 
